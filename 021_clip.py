d9ba043449be9c3fb64dc6853b1093a74102b8c3
Konrad Hafen <33733846+konradhafen@users.noreply.github.com>
2019-05-24 08:52:49 -0700

    Update 021_clip.py

--- 021_clip.py
+++ 021_clip.py
@@ -1,9 +1,14 @@
 import processing
 
+#set input and output file names
 polyPath = "C:/temp/buffer_clip.shp"
 linePath = "C:/temp/streams_clip.shp"
 clipPath = "C:/temp/clipped.shp"
 
+#run the clip tool
 processing.run("native:clip", {'INPUT':linePath,\
 'OVERLAY':polyPath,\
 'OUTPUT':clipPath})
+
+#add output to the qgis interface
+iface.addVectorLayer(clipPath, '', 'ogr')
